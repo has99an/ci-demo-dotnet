ebc1a8266ce497c208a3f8d14f42ce01d65caae6
Ahmad <10478728@ucn.dk>
2026-03-23 06:53:14 +0100

    Green scenario

--- CiDemoApp.Tests/DiscountCalculatorTests.cs
+++ CiDemoApp.Tests/DiscountCalculatorTests.cs
@@ -16,16 +16,6 @@ public class DiscountCalculatorTests
     }
 
     [Fact]
-    public void CalculateFinalPrice_With25PercentDiscount_ReturnsCorrectPrice()
-    {
-        var calculator = new DiscountCalculator();
-
-        decimal result = calculator.CalculateFinalPrice(200, 25);
-
-        Assert.Equal(150, result);
-    }
-
-    [Fact]
     public void CalculateFinalPrice_WithInvalidDiscount_ThrowsArgumentException()
     {
         var calculator = new DiscountCalculator();
